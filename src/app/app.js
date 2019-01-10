a820178db9362723f610fdba2aec52247d55fa01
Mariya Davydova <mariyafomkina@gmail.com>
2019-01-10 18:58:26 +0300

    Using ring components

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,10 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {render} from 'react-dom';
 import Link from '@jetbrains/ring-ui/components/link/link';
+import Heading, {H1, H2, H3, H4} from '@jetbrains/ring-ui/components/heading/heading';
+import Island, {Header, Content} from '@jetbrains/ring-ui/components/island/island';
+import Text from '@jetbrains/ring-ui/components/text/text';
+import Tooltip from '@jetbrains/ring-ui/components/tooltip/tooltip';
 
 import 'file-loader?name=[name].[ext]!../../manifest.json'; // eslint-disable-line import/no-unresolved
 import styles from './app.css';
@@ -145,64 +149,6 @@ class Widget extends Component {
 
   //-----RENDERING-DATA-----//
 
-  renderProblems(wf) {
-    if (wf.loading) {
-      return (
-        <p>Loading...</p>
-      )
-    } else {
-      return (
-        <div className={styles.widget}>
-          {Object.keys(wf.problems).map(key => (
-            <p key={key}>{wf.problems[key]}</p>
-          ))}
-        </div>
-      )
-    }
-  }
-
-  renderWorkflows(project) {
-    return (
-      <div className={styles.widget}>
-        {Object.keys(project.wfs).map(key => (
-          <div className={styles.widget} key={key}>
-            <h5 key={key}>{project.wfs[key].title ? project.wfs[key].title : project.wfs[key].name}</h5>
-            {this.renderProblems(project.wfs[key])}
-          </div>
-        ))}
-      </div>
-    )
-  }
-
-  renderProjects(yt) {
-    if (yt.brokenProjects) {
-      return (
-        <div className={styles.widget}>
-          {Object.keys(yt.brokenProjects).map(key => (
-            <div className={styles.widget} key={key}>
-              <Link
-                pseudo={false}
-                target={'_top'}
-                href={yt.url + '/admin/editProject/' + yt.brokenProjects[key].ringId + '?tab=workflow'}
-              >
-                <h4>{yt.brokenProjects[key].name}</h4>
-              </Link>
-              {this.renderWorkflows(yt.brokenProjects[key])}
-            </div>
-          ))}
-        </div>
-      )
-    } else if (yt.loading) {
-      return (
-        <h4>Loading...</h4>
-      )
-    } else {
-      return (
-        <h3>Horizon is clear!</h3>
-      )
-    };
-  }
-
   render() {
     const {data} = this.state;
 
@@ -211,7 +157,9 @@ class Widget extends Component {
         <div className={styles.widget}>
           {Object.keys(data).map(key => (
             <div className={styles.widget} key={key}>
-              <h3>{data[key].name} ({data[key].url})</h3>
+              <Tooltip title={data[key].url}>
+                <H2>{data[key].name}</H2>
+              </Tooltip>
               {this.renderProjects(data[key])}
             </div>
           ))}
@@ -220,10 +168,91 @@ class Widget extends Component {
     } else {
       return (
         <div className={styles.widget}>
-          <h3>Loading...</h3>
+          <H3 caps>Loading...</H3>
         </div>
       );
     }
+  }
+
+  renderProjects(yt) {
+    if (yt.brokenProjects) {
+      return (
+        <div className={styles.widget}>
+          {Object.keys(yt.brokenProjects).map(key => (
+            <div className={styles.widget} key={key}>
+              <Island>
+                <Header border>
+                  <Link
+                    pseudo={true}
+                    target={'_top'}
+                    href={this.projectSettingsUrl(yt, key)}
+                  >
+                    {yt.brokenProjects[key].name}
+                  </Link>
+                </Header>
+                <Content>
+                  {this.renderWorkflows(yt.brokenProjects[key])}
+                </Content>
+              </Island>
+            </div>
+          ))}
+        </div>
+      )
+    } else if (yt.loading) {
+      return (
+        <div className={styles.widget}>
+          <H3 caps>Loading...</H3>
+        </div>
+      )
+    } else {
+      return (
+        <div className={styles.widget}>
+          <H2 caps>Horizon is clear!</H2>
+        </div>
+      )
+    };
+  }
+
+  renderWorkflows(project) {
+    return (
+      <div className={styles.widget}>
+        {Object.keys(project.wfs).map(key => (
+          <div className={styles.widget} key={key}>
+            <H3>{this.wfTitle(project.wfs[key])}</H3>
+            {this.renderProblems(project.wfs[key])}
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  renderProblems(wf) {
+    if (wf.loading) {
+      return (
+        <div className={styles.widget}>
+          <Text info>Loading...</Text>
+        </div>
+      )
+    } else {
+      return (
+        <div className={styles.widget}>
+          <ul>
+            {Object.keys(wf.problems).map(key => (
+              <li key={key}><Text>{wf.problems[key]}</Text></li>
+            ))}
+          </ul>
+        </div>
+      )
+    }
+  }
+
+  wfTitle(project) {
+    return project.title ? project.title : project.name;
+  }
+
+  projectSettingsUrl(yt, projectId) {
+    return yt.url + '/admin/editProject/' +
+      yt.brokenProjects[projectId].ringId + '?tab=workflow';
   }
 }
 
